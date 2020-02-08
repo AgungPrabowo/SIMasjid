706ee118f5c60c007bdbff4749874404b28aca2a
AgungPrabowo <agungprabowo112@gmail.com>
2020-02-09 06:53:08 +0700

    ## v0.0.2

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import Login from "./src/screens/Login";
 
 export default class App extends Component {
   render() {
-    // return <Login/>
-    return <LoggedOut />;
+    return <Login/>
   }
 }
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,15 +7,36 @@ import {
   StyleSheet,
 } from 'react-native';
 import colors from '../styles/color';
+import InputField from '../components/form/InputField';
+import NextArrowButton from '../components/buttons/NextArrowButton';
 
 export default class Login extends Component {
   render() {
     return (
       <KeyboardAvoidingView style={styles.wrapper} behavior="padding">
-        <View styles={styles.wrapper}>
-          <ScrollView>
-            <Text>Login</Text>
+        <View style={styles.scrollViewWrapper}>
+          <ScrollView style={styles.avoidView}>
+            <Text style={styles.loginHeader}>Login</Text>
+            <InputField
+              labelText={'EMAIL ADDRESS'}
+              labelTextSize={14}
+              labelColor={colors.white}
+              textColor={colors.white}
+              borderBottomColor={colors.white}
+              inputType="email"
+              customStyle={{marginBottom: 30}}
+            />
+            <InputField
+              labelText="PASSWORD"
+              labelTextSize={14}
+              labelColor={colors.white}
+              textColor={colors.white}
+              borderBottomColor={colors.white}
+              inputType="password"
+              customStyle={{marginBottom: 30}}
+            />
           </ScrollView>
+          <NextArrowButton />
         </View>
       </KeyboardAvoidingView>
     );
@@ -27,5 +48,21 @@ const styles = StyleSheet.create({
     display: 'flex',
     flex: 1,
     backgroundColor: colors.green01,
+  },
+  scrollViewWrapper: {
+    marginTop: 70,
+    flex: 1,
+  },
+  avoidView: {
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 20,
+    flex: 1,
+  },
+  loginHeader: {
+    fontSize: 28,
+    color: colors.white,
+    fontWeight: '300',
+    marginBottom: 40,
   },
 });
